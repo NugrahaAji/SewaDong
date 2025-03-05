99b73e989fa8027458e714753ada9422aa1e3bfe
Nugraha Aji Saputra <151295979+NugrahaAji@users.noreply.github.com>
2025-03-05 09:18:54 +0700

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PinjemDong</title>
+    <title>SewaDong</title>
 
     <!-- Tailwind -->
     @vite('resources/css/app.css')
